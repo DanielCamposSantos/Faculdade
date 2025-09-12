52405d747dbb2bf5e82fd6d162abc50b8b7c0643
Daniel Campos <danielcampos.santos@ucsal.edu.br>
2025-09-12 15:29:15 -0300

    Exercicio 03 pronto

---
+++ Exercicio03/src/dominio/Pessoa.java
@@ -0,0 +1,13 @@
+package dominio;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    String profissao;
+
+    public Pessoa(String nome, int idade, String profissao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
+}
